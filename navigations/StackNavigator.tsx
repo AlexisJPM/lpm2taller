1e2943eb1a58b2fba5ff846ca72b7aa1cadd1656
Juan Romero <jr3994779@gmail.com>
2026-01-14 21:16:39 -0500

    actualizacion

--- navigations/StackNavigator.tsx
+++ navigations/StackNavigator.tsx
@@ -6,18 +6,12 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from '../screens/LoginScreen';
 import RegistroScreen from '../screens/RegistroScreen';
 import HomeScreen from '../screens/HomeScreen';
-<<<<<<< HEAD
-import { NavigationContainer } from '@react-navigation/native';
-
-const Stack = createStackNavigator()
-=======
 import GameScreen from '../screens/GameScreen';
 import ScoreScreen from '../screens/ScoreScreen';
 import PerfilScreen from '../screens/PerfilScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
->>>>>>> f4e404366374699bb682cbcfaaabe9bab73d4e34
 
 function MyStack() {
     return (
@@ -30,8 +24,6 @@ function MyStack() {
     );
 }
 
-<<<<<<< HEAD
-=======
 function MyTabs() {
     return (
         <Tab.Navigator>
@@ -42,16 +34,10 @@ function MyTabs() {
     );
 }
 
->>>>>>> f4e404366374699bb682cbcfaaabe9bab73d4e34
 export default function StackNav() {
     return (
         <NavigationContainer>
             <MyStack />
         </NavigationContainer>
     )
-<<<<<<< HEAD
 }
-
-=======
-}
->>>>>>> f4e404366374699bb682cbcfaaabe9bab73d4e34
