414ec713daee7d862fa387d97b2eae74e609ed9f
Juan Romero <jr3994779@gmail.com>
2026-01-21 20:58:03 -0500

    Registro con autenticacion

--- screens/RegistroScreen.tsx
+++ screens/RegistroScreen.tsx
@@ -1,22 +1,39 @@
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Alert, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { supabase } from '../supabase/config'
 
-export default function RegistroScreen() {
- const [cedula, setcedula] = useState("")
+export default function RegistroScreen({navigation} : any) {
+ 
      const [nombre, setnombre] = useState("")
-     const [contrasenia, setcontrasenia] = useState("")
-     const [correo, setcorreo] = useState("")
+     const [password, setpassword] = useState("")
+     const [email, setemail] = useState("")
      const [edad, setedad] = useState(0)
      
- async function guardar(){
+     async function registro(){
+    const { data, error } = await supabase.auth.signUp({
+     email: email,
+    password: password,
+})
+
+
+if(data.user != null){
+navigation.navigate("Login")
+guardar( data.user.id)
+}else{
+    console.log(error);
+      
+      Alert.alert("Error", error?.message)
+}
+
+
+  }
+ async function guardar(uid : string){
     const { error } = await supabase
   .from('usuario')
   .insert({ 
-    cedula: cedula,
+    uid : uid,
     nombre: nombre, 
-    contrasenia: contrasenia ,
-    correo: correo,
+    email : email,
     edad: edad
     })
     console.log(error);
@@ -33,12 +50,7 @@ export default function RegistroScreen() {
              </View>
        
  
-   <TextInput
-     placeholder='Ingresar su cedula'
- style={styles.input}
- onChangeText={(texto)=> setcedula(texto) }
- value = {cedula}
-   />
+   
     <TextInput
  placeholder='Ingrese su nombre'
  style={styles.input}
@@ -48,8 +60,8 @@ export default function RegistroScreen() {
      <TextInput
  placeholder='Cree su contraseña'
  style={styles.input}
- onChangeText={(texto)=> setcontrasenia(texto) }
- value = {contrasenia}
+ onChangeText={(texto)=> setpassword(texto) }
+ value = {password}
  
      />
         <TextInput
@@ -62,12 +74,12 @@ export default function RegistroScreen() {
       <TextInput
  placeholder='Ingresar su correo'
  style={styles.input}
- onChangeText={(texto)=> setcorreo(texto) }
- value= {correo}
+ onChangeText={(texto)=> setemail(texto) }
+ value= {email}
       />
      
  
-     <TouchableOpacity style = {styles.button} onPress={guardar} >
+     <TouchableOpacity style = {styles.button} onPress={registro} >
          <Text >Guardar</Text>
      </TouchableOpacity>
      </View>
